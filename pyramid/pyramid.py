a72d7f30fcb9a55ee4ec33cc82bb0a8debc13c15
sardred <andelar96@gmail.com>
2017-10-04 16:29:15 +0200

    Sliding-window image pyramid tutorial with pip requirements

---
+++ pyramid/pyramid.py
@@ -0,0 +1,63 @@
+# USAGE
+# python pyramid.py --image images/rubberducky.jpg
+
+import argparse
+import time
+import cv2
+import imutils
+
+
+def pyramid(image, scale=1.5, min_size=(30, 30)):
+    # yield the original image
+    yield image
+
+    # keep looping over the pyramid
+    while True:
+        # compute the new dimensions of the image and resize it
+        w = int(image.shape[1] / scale)
+        image = imutils.resize(image, width=w)
+
+        # if the resized image does not meet the supplied minimum
+        # size, then stop constructing the pyramid
+        if image.shape[0] < min_size[1] or image.shape[1] < min_size[0]:
+            break
+
+        # yield the next image in the pyramid
+        yield image
+
+
+def sliding_window(image, step_size, window_size):
+    # slide a window across the image
+    for y in range(0, image.shape[0], step_size):
+        for x in range(0, image.shape[1], step_size):
+            # yield the current window
+            yield (x, y, image[y:y + window_size[1], x:x + window_size[0]])
+
+
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True, help="Path to the image")
+args = vars(ap.parse_args())
+
+# load the image and define the window width and height
+image = cv2.imread(args["image"])
+(winW, winH) = (128, 128)
+
+# loop over the image pyramid
+for resized in pyramid(image, scale=1.5):
+    # loop over the sliding window for each layer of the pyramid
+    for (x, y, window) in sliding_window(resized, step_size=32, window_size=(winW, winH)):
+        # if the window does not meet our desired window size, ignore it
+        if window.shape[0] != winH or window.shape[1] != winW:
+            continue
+
+        # THIS IS WHERE YOU WOULD PROCESS YOUR WINDOW, SUCH AS APPLYING A
+        # MACHINE LEARNING CLASSIFIER TO CLASSIFY THE CONTENTS OF THE
+        # WINDOW
+
+        # since we do not have a classifier, we'll just draw the window
+        clone = resized.copy()
+        cv2.rectangle(clone, (x, y), (x + winW, y + winH), (0, 255, 0), 2)
+        cv2.imshow("Window", clone)
+        cv2.waitKey(1)
+        time.sleep(0.005)
