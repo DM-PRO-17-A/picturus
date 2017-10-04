9e4f827378f41e703c3109170d2c7f6c795df552
eirikhl <eirikhl@stud.ntnu.no>
2017-10-04 14:35:12 +0200

    Added pictures

--- simulation/test.py
+++ simulation/test.py
@@ -2,4 +2,8 @@ from PIL import Image
 from matplotlib.pyplot import imshow
 import numpy as np
 
-
+img = Image.open("pics/50.jpg")
+img = img.convert("L")
+img = np.asarray(img)
+% matplotlib inline
+plt.show(img, cmap='gray')
