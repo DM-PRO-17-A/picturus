ada160a0952ac398f8f2aab74849079257ca78e7
Ane Hov <anenhov@gmail.com>
2017-10-04 16:04:02 +0200

    useful commit message

--- simulation/test.py
+++ simulation/test.py
@@ -1,9 +1,12 @@
 from PIL import Image
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
 from matplotlib.pyplot import imshow
 import numpy as np
 
 img = Image.open("pics/50.jpg")
 img = img.convert("L")
 img = np.asarray(img)
-% matplotlib inline
-plt.show(img, cmap='gray')
+plt.imshow(img, cmap='gray')
+plt.show();
