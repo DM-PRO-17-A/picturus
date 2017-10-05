606fab4234ba80e69799848fc8035c43aea34512
Ane Hov <anenhov@gmail.com>
2017-10-05 14:34:00 +0200

    more test pictures and notes

--- simulation/test.py
+++ simulation/test.py
@@ -67,10 +67,12 @@ def gtsrb_predict(img):
 #img2 = prepare_gtsrb(Image.open("pics/left.jpg"))
 #img3 = prepare_gtsrb(Image.open("pics/right.jpg"))
 #img4 = prepare_gtsrb(Image.open("pics/stop.jpg"))
-img5 = prepare_gtsrb(Image.open("test_pics/stop95.jpg"))
+img5 = prepare_gtsrb(Image.open("test_pics/stoptest2.jpg"))
+img6 = prepare_gtsrb(Image.open("test_pics/stoptest3.jpg"))
 
 #gtsrb_predict(img)
 #gtsrb_predict(img2)
 #gtsrb_predict(img3)
 #gtsrb_predict(img4)
 gtsrb_predict(img5)
+gtsrb_predict(img6)
