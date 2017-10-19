9704f408a1853ee540ec1a0b5513fd643b17718e
Ane Hov <anenhov@gmail.com>
2017-10-19 15:30:29 +0200

    results printet to file

--- simulation/cortex.py
+++ simulation/cortex.py
@@ -8,6 +8,30 @@ from time import sleep
 from random import randrange
 
 
+# Code example of how to get all files from a directory
+# We want to look at all the pictures taken at a given time
+from os import walk
+#path = "pics/"
+#path = "test_pics/test/"
+path = "../../../Downloads/GTSRB/Final_Test/Images/"
+
+pics = []
+for (dirpath, dirnames, filenames) in walk(path):
+    for filename in filenames:
+        pics.append(path + filename)
+    break
+#pics = []
+#filename_base = 'test'
+#for i in range(15):
+#    filename = filename_base + str(i) + '.jpg'
+#    pics.append(path + filename)
+
+
+
+# Use with average
+average_result = [0] * 43
+# Update for each picture, each frame?
+# How to decay?
 
 # Maybe install termcolor for colored output?
 # from termcolor import colored
@@ -21,11 +45,13 @@ def open_images(pics):
     return map(lambda p: (p, prepare_gtsrb(Image.open(p))), pics)
 
 
+pics = open_images(pics)
+
 # TODO: give images more meaningful file names
-test_case_normal_signs = open_images(("pics/50.jpg", "pics/left.jpg", "pics/right.jpg", "pics/stop.jpg"))
-test_case_zoom = open_images(("test_pics/stoptest.jpg", "test_pics/stoptest1.jpg", "test_pics/stoptest2.jpg", "test_pics/stoptest3.jpg"))
-test_case_partially_covered = open_images(("test_pics/stop75.jpg", "test_pics/stop85.jpg", "test_pics/stop90.jpg", "test_pics/stop95.jpg"))
-tests = (test_case_normal_signs, test_case_zoom, test_case_partially_covered)
+#test_case_normal_signs = open_images(("pics/50.jpg", "pics/left.jpg", "pics/right.jpg", "pics/stop.jpg"))
+#test_case_zoom = open_images(("test_pics/stoptest.jpg", "test_pics/stoptest1.jpg", "test_pics/stoptest2.jpg", "test_pics/stoptest3.jpg"))
+#test_case_partially_covered = open_images(("test_pics/stop75.jpg", "test_pics/stop85.jpg", "test_pics/stop90.jpg", "test_pics/stop95.jpg"))
+#tests = (test_case_normal_signs, test_case_zoom, test_case_partially_covered)
 
 
 # # TODO: improve readability of output
@@ -34,15 +60,16 @@ tests = (test_case_normal_signs, test_case_zoom, test_case_partially_covered)
 #         print pic[0]
 #         gtsrb_predict(pic[1])
 
-
+count = 0
 while True:
-    break
+    #break
     print "Drive"
     sleep(3)
 
     print "Get random picture"
     fsm = fsm_states[1]
-    pic = test_case_normal_signs[randrange(0, len(test_case_normal_signs))]
+    pic = pics[randrange(0, len(pics))]
+    #pic = pics[count]
     print "The chosen picture is " + pic[0]
 
     print "Send to QNN"
@@ -55,21 +82,10 @@ while True:
         print "Wait for daughter card to perform action"
         pcb.receive()
         print "Action performed"
+    
+    count += 1
+    #if count > 14:
+    #    break
+
 
         
-# Code example of how to get all files from a directory
-# We want to look at all the pictures taken at a given time
-from os import walk
-path = "pics/"
-
-pics = []
-for (dirpath, dirnames, filenames) in walk(path):
-    for filename in filenames:
-        pics.append(path + filename)
-    break
-print pics
-
-# Use with average
-average_result = [0] * 43
-# Update for each picture, each frame?
-# How to decay?
