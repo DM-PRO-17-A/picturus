042ade5a5befdfeb3ee5cab549b068bf53d3fce3
eirikhl <eirikhl@stud.ntnu.no>
2017-11-15 15:43:35 +0100

    Commit to have the minor changes available

--- main/main.cpp
+++ main/main.cpp
@@ -1,6 +1,20 @@
-namespace Main;
+#include <vector>
+#include <algorithm> // for std::fill
+
 
 int main()
 {
+	float[43] average;
+	int i;
+	for ( i = 0, i < 43; ++i )
+		average[i] = 0.0;
+
+	// Initialise camera program here? Or separately in Bash?
+
+	while(1)
+	{
+		// Read from 
+	}
+	
 	return 0;
 }
